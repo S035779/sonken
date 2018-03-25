dda9831c66fb7c0ef1800ad19469b75d5aa1ca26
Mamoru Hashimoto <you@example.com>
2018-03-25 13:11:43 +0900

    modified install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -3,23 +3,17 @@ ASSETS=/app/assets
 TIME=$(date "+%Y/%m/%dT+%T.%3N")
 TODAY=$(date "+%Y%m%d")
 
-if[! -e $ASSETS/js/app.bundle.js ]; then
-  echo "$TIME [WARN] File not found."
-else
+if[-e $ASSETS/js/app.bundle.js ]; then
   mv $ASSETS/js/app.bundle.js     $ASSETS/js/app.bundle_${TODAY}.js
   mv $ASSETS/js/app.bundle.js.map $ASSETS/js/app.bundle_${TODAY}.js.map
 fi
 
-if[! -e $ASSETS/js/view.bundle.js ]; then
-  echo "$TIME [WARN] File not found."
-else
+if[-e $ASSETS/js/view.bundle.js ]; then
   mv $ASSETS/js/view.bundle.js     $ASSETS/js/view.bundle_${TODAY}.js
   mv $ASSETS/js/view.bundle.js.map $ASSETS/js/view.bundle_${TODAY}.js.map
 fi
 
-if[! -e $ASSETS/js/icon.bundle.js ]; then
-  echo "$TIME [WARN] File not found."
-else
+if[-e $ASSETS/js/icon.bundle.js ]; then
   mv $ASSETS/js/icon.bundle.js     $ASSETS/js/icon.bundle_${TODAY}.js
   mv $ASSETS/js/icon.bundle.js.map $ASSETS/js/icon.bundle_${TODAY}.js.map
 fi
@@ -27,10 +21,8 @@ fi
 cp dist/*.js $ASSETS/js/
 cp dist/*.js.map $ASSETS/js/
 
-if[! -e $ASSETS/js/icon.bundle.js ]; then
-  echo "$TIME [WARN] File not found."
-else
-mv $ASSETS/image/favicon.ico $ASSETS/image/favicon_${TODAY}.ico
+if[-e $ASSETS/js/icon.bundle.js ]; then
+  mv $ASSETS/image/favicon.ico $ASSETS/image/favicon_${TODAY}.ico
 fi
 
 cp dist/*.ico $ASSETS/image/
