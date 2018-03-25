fd111679bdca8945e6665ba7655c604b5d8781ef
Mamoru Hashimoto <you@example.com>
2018-03-25 15:31:20 +0900

    modified install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -5,9 +5,14 @@ WORKDIR=/app/sonken
 if [ ! -d $ASSETS/js ]; then
     mkdir $ASSETS/js
 fi
-cp $WORKDIR/dist/*.bundle.js $WORKDIR/dist/*.bundle.js.map $ASSETS/js/
+cp $WORKDIR/dist/app.bundle.js      $ASSETS/js/
+cp $WORKDIR/dist/app.bundle.js.map  $ASSETS/js/
+cp $WORKDIR/dist/icon.bundle.js     $ASSETS/js/
+cp $WORKDIR/dist/icon.bundle.js.map $ASSETS/js/
+cp $WORKDIR/dist/view.bundle.js     $ASSETS/js/
+cp $WORKDIR/dist/view.bundle.js.map $ASSETS/js/
 
 if [ ! -d $ASSETS/image ]; then
     mkdir $ASSETS/image
 fi
-cp $WORKDIR/dist/*.ico $ASSETS/image/
+cp $WORKDIR/dist/favicon.ico $ASSETS/image/
