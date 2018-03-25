cbdb4400fff51ef1d9d67bf1e6830fa7349d2435
Mamoru Hashimoto <you@example.com>
2018-03-25 13:45:36 +0900

    modified install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 ASSETS=/app/assets
-TIME=$(date "+%Y/%m/%dT+%T.%3N")
-TODAY=$(date "+%Y%m%d")
-
-if[-f $ASSETS/js/app.bundle.js ]; then
-  mv $ASSETS/js/app.bundle.js     $ASSETS/js/app.bundle_${TODAY}.js
-  mv $ASSETS/js/app.bundle.js.map $ASSETS/js/app.bundle_${TODAY}.js.map
-fi
-
-if[-f $ASSETS/js/view.bundle.js ]; then
-  mv $ASSETS/js/view.bundle.js     $ASSETS/js/view.bundle_${TODAY}.js
-  mv $ASSETS/js/view.bundle.js.map $ASSETS/js/view.bundle_${TODAY}.js.map
-fi
-
-if[-f $ASSETS/js/icon.bundle.js ]; then
-  mv $ASSETS/js/icon.bundle.js     $ASSETS/js/icon.bundle_${TODAY}.js
-  mv $ASSETS/js/icon.bundle.js.map $ASSETS/js/icon.bundle_${TODAY}.js.map
-fi
-
-cp dist/*.js $ASSETS/js/
-cp dist/*.js.map $ASSETS/js/
-
-if[-f $ASSETS/js/icon.bundle.js ]; then
-  mv $ASSETS/image/favicon.ico $ASSETS/image/favicon_${TODAY}.ico
-fi
-
+mkdir $ASSETS/js $ASSETS/image
+cp dist/*.bundle.js dist/*.bundle.js.map $ASSETS/js/
 cp dist/*.ico $ASSETS/image/
