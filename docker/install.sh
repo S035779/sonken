30611185a025b2057c2473d79471732ed28638f4
Mamoru Hashimoto <you@example.com>
2018-03-25 13:05:16 +0900

    modified install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
 ASSETS=/app/assets
+TIME=$(date "+%Y/%m/%dT+%T.%3N")
 TODAY=$(date "+%Y%m%d")
 
-mv $ASSETS/js/app.bundle.js        $ASSETS/js/app.bundle_${TODAY}.js
-mv $ASSETS/js/app.bundle.js.map    $ASSETS/js/app.bundle_${TODAY}.js.map
-mv $ASSETS/js/view.bundle.js        $ASSETS/js/view.bundle_${TODAY}.js
-mv $ASSETS/js/view.bundle.js.map    $ASSETS/js/view.bundle_${TODAY}.js.map
-mv $ASSETS/js/icon.bundle.js        $ASSETS/js/icon.bundle_${TODAY}.js
-mv $ASSETS/js/icon.bundle.js.map    $ASSETS/js/icon.bundle_${TODAY}.js.map
+if[! -e $ASSETS/js/app.bundle.js ]; then
+  echo "$TIME [WARN] File not found."
+else
+  mv $ASSETS/js/app.bundle.js     $ASSETS/js/app.bundle_${TODAY}.js
+  mv $ASSETS/js/app.bundle.js.map $ASSETS/js/app.bundle_${TODAY}.js.map
+fi
+
+if[! -e $ASSETS/js/view.bundle.js ]; then
+  echo "$TIME [WARN] File not found."
+else
+  mv $ASSETS/js/view.bundle.js     $ASSETS/js/view.bundle_${TODAY}.js
+  mv $ASSETS/js/view.bundle.js.map $ASSETS/js/view.bundle_${TODAY}.js.map
+fi
+
+if[! -e $ASSETS/js/icon.bundle.js ]; then
+  echo "$TIME [WARN] File not found."
+else
+  mv $ASSETS/js/icon.bundle.js     $ASSETS/js/icon.bundle_${TODAY}.js
+  mv $ASSETS/js/icon.bundle.js.map $ASSETS/js/icon.bundle_${TODAY}.js.map
+fi
+
 cp dist/*.js $ASSETS/js/
 cp dist/*.js.map $ASSETS/js/
 
+if[! -e $ASSETS/js/icon.bundle.js ]; then
+  echo "$TIME [WARN] File not found."
+else
 mv $ASSETS/image/favicon.ico $ASSETS/image/favicon_${TODAY}.ico
+fi
+
 cp dist/*.ico $ASSETS/image/
