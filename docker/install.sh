fc7b296adfeaceda5cda47e523f7fdd6d80d2a9d
Mamoru Hashimoto <you@example.com>
2018-03-25 14:16:03 +0900

    modified install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ASSETS=/app/assets
 
-if[! -d $ASSETS/js ]; then
+if[ ! -d $ASSETS/js ]; then
     mkdir $ASSETS/js
 fi
 cp dist/*.bundle.js dist/*.bundle.js.map $ASSETS/js/
 
-if[! -d $ASSETS/image ]; then
+if[ ! -d $ASSETS/image ]; then
     mkdir $ASSETS/image
 fi
 cp dist/*.ico $ASSETS/image/
