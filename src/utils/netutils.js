f08271b840ad8c63d6426238d264a988e012daf3
mamoru_hashimoto <you@example.com>
2018-08-21 01:01:31 +0900

    modified src/utils/netutils.js

--- src/utils/netutils.js
+++ src/utils/netutils.js
@@ -18,7 +18,7 @@ const min = 20000;
 const max = 50000;
 const retry = () => Math.floor(Math.random() * (max - min + 1) + min);
 
-const promiseThrottle = new PromiseThrottle({ requestsPerSecond: 0.1, promiseImplementation: Promise });
+const promiseThrottle = new PromiseThrottle({ requestsPerSecond: 1, promiseImplementation: Promise });
 
 const throttle = (url, options) => promiseThrottle.add(promise.bind(this, url, options));
 
