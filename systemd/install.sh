e3023b0178821d8a82ed61b7675164317eed6387
mamoru_hashimoto <you@example.com>
2018-08-22 22:06:19 +0900

    modified package.json

--- systemd/install.sh
+++ systemd/install.sh
@@ -6,11 +6,8 @@ if [ ! -d $ASSET/js ]; then
     mkdir $ASSET/js
 fi
 cp $WORKDIR/dist/app.bundle.js      $ASSET/js/
-cp $WORKDIR/dist/app.bundle.js.map  $ASSET/js/
 cp $WORKDIR/dist/icon.bundle.js     $ASSET/js/
-cp $WORKDIR/dist/icon.bundle.js.map $ASSET/js/
 cp $WORKDIR/dist/view.bundle.js     $ASSET/js/
-cp $WORKDIR/dist/view.bundle.js.map $ASSET/js/
 
 if [ ! -d $ASSET/image ]; then
     mkdir $ASSET/image
