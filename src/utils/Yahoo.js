c36f3883e1d6f8b37ab2a2ae5db5534764aa97cc
mamoru_hashimoto <you@example.com>
2018-11-18 20:50:02 +0900

    modified ../../src/utils/Yahoo.js

--- src/utils/Yahoo.js
+++ src/utils/Yahoo.js
@@ -220,8 +220,8 @@ class Yahoo {
             const setExplan     = __obj => __obj ? __obj.string : '';
             const isExplan      = __obj => __obj.name === '商品詳細';
             const explanation   = R.compose(setExplan, R.find(isExplan), R.map(setSubStr))(pairs);
-            const str2none      = R.replace(/支払詳細|商品詳細|発送詳細|注意事項/g, '');
-            return R.merge(_obj, { explanation: str2none(explanation) }); 
+            //const str2none      = R.replace(/支払詳細|商品詳細|発送詳細|注意事項/g, '');
+            return R.merge(_obj, { explanation }); 
           };
           const setImages       = _obj => R.merge(_obj, { images: R.uniq(_obj.images) });
           const isGuid          = _obj => _obj.guid__;
@@ -382,9 +382,9 @@ class Yahoo {
             const setSubStr     = __obj => R.merge(__obj, { string: subString(__obj) });
             const setExplan     = __obj => __obj ? __obj.string : '';
             const isExplan      = __obj => __obj.name === '商品詳細';
-            const explanation = R.compose(str2none, setExplan, R.find(isExplan), R.map(setSubStr))(pairs);
-            const str2none      = R.replace(/支払詳細|商品詳細|発送詳細|注意事項/g, '');
-            return R.merge(_obj, { explanation: str2none(explanation) }); 
+            const explanation = R.compose(setExplan, R.find(isExplan), R.map(setSubStr))(pairs);
+            //const str2none      = R.replace(/支払詳細|商品詳細|発送詳細|注意事項/g, '');
+            return R.merge(_obj, { explanation }); 
           };
           const setImages       = _obj => R.merge(_obj, { images: R.uniq(_obj.images) });
           const isGuid          = _obj => _obj.guid__;
